dfe1aed2d521049c2e5ebf2f59b7fc475c9b4971
NeChelovekAKek <mr.dr.kali.linux@gmail.com>
2025-03-25 22:36:44 +0300

    remove script drag and drop

--- DeckManager.cs
+++ DeckManager.cs
@@ -1,67 +1,40 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeckManager : MonoBehaviour
 {
     public GameObject cardPrefab; // Префаб карты
-    public Transform deckPosition; // Где лежит колода
-    public int deckSize = 20; // Количество карт
-    public Player playerHand; // Ссылка на игрока
-    private List<GameObject> deck = new List<GameObject>(); // Список карт
+    public Transform handZone; // Где появляются карты (ссылка на Hand)
+    public int maxHandSize = 5; // Максимальное количество карт в руке
+    private List<GameObject> hand = new List<GameObject>(); // Карты в руке
 
-    void Start()
+    private int totalCards = 20; // Количество карт в колоде
+
+    void OnMouseDown() // Клик по колоде
     {
-        GenerateDeck();
-        ShuffleDeck();
+        DrawCards();
     }
 
-    void OnMouseDown() // Нажатие по колоде
+    public void DrawCards()
     {
-        DrawCard();
-    }
-
-    public void DrawCard()
-    {
-        if (deck.Count > 0 && playerHand.hand.Count < playerHand.maxHandSize)
+        if (hand.Count >= maxHandSize)
         {
-            GameObject newCard = deck[deck.Count - 1]; // Берём верхнюю карту
-            deck.RemoveAt(deck.Count - 1); // Убираем её из колоды
-            playerHand.AddCardToHand(newCard);
-            newCard.SetActive(false);
-
-            // Присваиваем рандомный ранг
-            Card cardComponent = newCard.GetComponent<Card>();
-            if (cardComponent != null)
-            {
-                cardComponent.AssignRandomRank();
-                Debug.Log($"Выдана карта с рангом: {cardComponent.rank}");
-            }
+            Debug.Log("Рука заполнена!");
+            return;
         }
-        else
-        {
-            Debug.Log("Колода пуста или рука заполнена.");
-        }
-    }
 
-    private void GenerateDeck() // Создаём колоду
-    {
-        for (int i = 0; i < deckSize; i++)
+        int cardsToDraw = Mathf.Min(5 - hand.Count, totalCards); // Сколько можно взять
+        for (int i = 0; i < cardsToDraw; i++)
         {
-            GameObject card = Instantiate(cardPrefab, deckPosition.position + new Vector3(0, i * 0.02f, 0), Quaternion.identity);
-            card.transform.SetParent(deckPosition);
-            card.SetActive(true); // Прячем карту
-            deck.Add(card);
-        }
-    }
+            GameObject newCard = Instantiate(cardPrefab, transform.position, Quaternion.identity);
+            newCard.transform.SetParent(handZone); // Помещаем в руку
+            hand.Add(newCard);
+            totalCards--; // Уменьшаем колоду
 
-    private void ShuffleDeck() // Перемешивание
-    {
-        for (int i = 0; i < deck.Count; i++)
-        {
-            GameObject temp = deck[i];
-            int randomIndex = Random.Range(i, deck.Count);
-            deck[i] = deck[randomIndex];
-            deck[randomIndex] = temp;
+            // Размещаем карту с отступом
+            float offset = 2.0f;
+            newCard.transform.position = new Vector3(handZone.position.x + (hand.Count - 1) * offset, handZone.position.y, handZone.position.z);
         }
     }
 }
